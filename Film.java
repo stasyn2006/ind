b2561b2fe42b3a27637a38b603287a3221305d91
stasyn2006 <168012209+stasyn2006@users.noreply.github.com>
2024-05-01 16:15:03 +0300

    Add files via upload

---
+++ Film.java
@@ -0,0 +1,13 @@
+public class Film {
+    String name;
+    int releaseYear;
+    String director;
+    double rating;
+
+    public Film(String name, int releaseYear, String director, double rating) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.director = director;
+        this.rating = rating;
+    }
+}
---
+++ Shooting.java
@@ -0,0 +1,13 @@
+public class Shooting {
+    long Price;
+    int Actors;
+    int Film_crew;
+    int Producers;
+
+    public Shooting(long Price, int Actors, int Film_crew, int Producers) {
+        this.Price = Price;
+        this.Actors = Actors;
+        this.Film_crew = Film_crew;
+        this.Producers = Producers;
+    }
+}
